e98a828c4d35ab6ba0097c1b3b590fc3fd4ff18e
Shahbaz Khan <khan.shahbaz4595@gmail.com>
2023-12-02 22:32:28 +0100

    Chapter-3: Added basic jsx

--- React A to Z/Chapter-3/app.js
+++ React A to Z/Chapter-3/app.js
@@ -1,2 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+const name = 'Shahbaz'
+const jsxHeading = <h1>Hey {name}, Welcome to React A to Z</h1>
+
+console.log(jsxHeading)
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(jsxHeading)
